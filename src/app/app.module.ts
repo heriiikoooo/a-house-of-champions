67e93c877cbd6ec1e064f1c851b3ee3a902ae31e
Jericho <jerichomanalo140@gmail.com>
2024-01-12 06:34:04 +0800

    Vision, Mission, core values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { VisionComponent } from './components/vision/vision.component';
+import { MissionComponent } from './components/mission/mission.component';
+import { CoreValuesComponent } from './components/core-values/core-values.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VisionComponent,
+    MissionComponent,
+    CoreValuesComponent
   ],
   imports: [
     BrowserModule
