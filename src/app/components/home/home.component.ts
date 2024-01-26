709015dcffde4310cf46f29d4b3f31f1b0296060
Jericho <jerichomanalo140@gmail.com>
2024-01-27 03:09:28 +0800

    updates

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,44 +10,83 @@ export class HomeComponent implements OnInit {
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    const scrollPos = window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log(scrollPos)
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // this.runScrollListener(scrollPos);
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll(){
     const scrollPos = window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if(scrollPos > 10 && scrollPos < 20) {
-      console.log('change na')
-    // this.renderer.addClass(this.el.nativeElement.querySelector('#ahoc-navbar'), 'border')
-    this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'position', 'fixed')
-      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'rgba(255, 255, 255, 0.300)')
+    this.runScrollListener(scrollPos);
+  }
+
+  runScrollListener(scrollPos:any) {
+    console.log('position : '  + scrollPos)
+    if(scrollPos < 120) { 
+      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'position', 'absolute');
+      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'transparent');
+      this.renderer.setStyle(this.el.nativeElement.querySelector ('#toggler-icon'), 'background', `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='white' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`);
+
+      const navitems: HTMLElement = this.el.nativeElement;
+      navitems.querySelectorAll('.animated-items').forEach((domElement) => {
+        domElement.classList.remove('fill-text-color-pattele');
+      })
+
+      const dom: HTMLElement = this.el.nativeElement;
+      dom.querySelectorAll('.header-social-icons .items svg  path').forEach((domElement) => {
+        domElement.classList.remove('fill-icons-black');
+      })
     }
-    else if(scrollPos > 20 && scrollPos < 30) {
-      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'rgba(255, 255, 255, 0.600)')
+
+    if(scrollPos > 120) {
+      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'position', 'fixed');
+
+      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'rgba(255, 255, 255, 0.'+(scrollPos + 100)+')');
+      this.renderer.setStyle(this.el.nativeElement.querySelector ('#toggler-icon'), 'background', `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='white' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`);
+
+      const navitems: HTMLElement = this.el.nativeElement;
+      navitems.querySelectorAll('.animated-items').forEach((domElement) => {
+        domElement.classList.remove('fill-text-color-pattele');
+      })
+
+      const dom: HTMLElement = this.el.nativeElement;
+      dom.querySelectorAll('.header-social-icons .items svg  path').forEach((domElement) => {
+        domElement.classList.remove('fill-icons-black');
+      })
     }
-    else if(scrollPos > 30 && scrollPos < 50) {
-      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'rgba(255, 255, 255, 0.800)')
-    }
-    else if(scrollPos > 50) {
-      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'white')
-    }
-    else {
-      this.renderer.setStyle(this.el.nativeElement.querySelector('#ahoc-navbar'), 'background', 'transparent')
+   
+    if(scrollPos > 190) {
+      this.renderer.setStyle(this.el.nativeElement.querySelector ('#ahoc-navbar'), 'background', 'white');
+      this.renderer.setStyle(this.el.nativeElement.querySelector ('#toggler-icon'), 'background', `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(123, 18, 30, 1)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`);
+
+      const navitems: HTMLElement = this.el.nativeElement;
+      navitems.querySelectorAll('.animated-items').forEach((domElement) => {
+        domElement.classList.add('fill-text-color-pattele');
+      })
+
+      const dom: HTMLElement = this.el.nativeElement;
+      const elements = dom.querySelectorAll('.header-social-icons .items svg  path');
+      elements.forEach((domElement) => {
+        domElement.classList.add('fill-icons-black');
+      })
     }
   }
 
   scrollToTarget(id:string) {
     let targetEl =  this.el.nativeElement.querySelector('#'+id)
-      console.log(targetEl)
+    console.log(targetEl)
 
-      if(targetEl) {
-        targetEl.scrollIntoView(
-          {
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest'
-          }
-        )
-      }
+    if(targetEl) {
+      targetEl.scrollIntoView(
+        {
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'nearest'
+        }
+      )
+    }
     
   }
 
